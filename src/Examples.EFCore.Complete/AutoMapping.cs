7429cde768dd61bf172deabb2e77fcdeb06f09ae
Kelly Stuard <kellystuard@gmail.com>
2020-04-26 00:20:22 -0500

    Additional documentation.

--- src/Examples.EFCore.Complete/AutoMapping.cs
+++ src/Examples.EFCore.Complete/AutoMapping.cs
@@ -2,8 +2,14 @@
 
 namespace Examples.EFCore.Complete
 {
+	/// <summary>
+	/// Provides to AutoMapper a named configuration for maps.
+	/// </summary>
 	public class AutoMapping : Profile
 	{
+		/// <summary>
+		/// Creates an instance of <see cref="AutoMapping"/>.
+		/// </summary>
 		public AutoMapping()
 		{
 			CreateMap<Models.User, Data.User>()
